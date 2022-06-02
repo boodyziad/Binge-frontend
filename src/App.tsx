2b459511e7b4851b8202c444eda2a52e1832d106
Abdallah Ziad <boodyziad1@gmail.com>
2022-06-02 11:57:41 +0200

    Add autocomplete capability to input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Input from './components/Input';
 
@@ -8,10 +6,37 @@ function App() {
     console.log(text);
   };
 
+  const handleAutocompleteSelect = (id: number) => {
+    console.log(id);
+  };
+
   return (
     <div className='App'>
       <div className='container'>
-        <Input onChange={handleInputChange} />
+        <Input
+          onChange={handleInputChange}
+          onSelectItem={handleAutocompleteSelect}
+          autoCompleteItems={[
+            {
+              id: 1,
+              title: "Marvel's Daredevil",
+              backdrop_url:
+                'https://image.tmdb.org/t/p/original/gLACGQiS8kRP2hDnzdmIYo2uRRc.jpg',
+            },
+            {
+              id: 2,
+              title: "Marvel's Jessica Jones",
+              backdrop_url:
+                'https://image.tmdb.org/t/p/original/fjEOQhzZk2Or7VYUBeMx5ZIwU95.jpg',
+            },
+            {
+              id: 3,
+              title: "Marvel's The Defenders",
+              backdrop_url:
+                'https://image.tmdb.org/t/p/original/n4XLn0wLCxWSFaQgG6queZlFPKi.jpg',
+            },
+          ]}
+        />
       </div>
     </div>
   );
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,47 @@
 import React, { useState } from 'react';
 
-interface InputProps {
-  onChange: (text: string) => void;
+interface AutoCompleteItem {
+  id: number;
+  title: string;
+  backdrop_url: string;
 }
 
-function Input({ onChange }: InputProps) {
+interface InputProps {
+  onChange: (text: string) => void;
+  autoCompleteItems: AutoCompleteItem[];
+  onSelectItem: (id: number) => void;
+}
+
+function Input({ onChange, autoCompleteItems, onSelectItem }: InputProps) {
   const [name, setName] = useState('');
 
   return (
-    <div className='input-container'>
-      <input
-        type='text'
-        className='input'
-        value={name}
-        onChange={(e) => {
-          setName(e.target.value);
-          onChange(e.target.value);
-        }}
-      />
+    <div>
+      <div className='input-container'>
+        <input
+          type='text'
+          className='input'
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+            onChange(e.target.value);
+          }}
+        />
+      </div>
+      <div className='autocomplete'>
+        {autoCompleteItems.map(({ id, title, backdrop_url }) => (
+          <div
+            key={id}
+            onClick={() => onSelectItem(id)}
+            className='autocomplete-item'
+          >
+            <img src={backdrop_url} className='autocomplete-backdrop' />
+            <div className='autocomplete-body'>
+              <span className='autocomplete-title'>{title}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
