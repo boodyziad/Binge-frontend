527222977380effdff8fbaa69104345f90574b8b
Abdallah Ziad <boodyziad1@gmail.com>
2022-06-03 01:08:16 +0200

    Prepare project for deployment

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,5 +1,5 @@
 export default {
   baseUrl: 'https://api.themoviedb.org/3',
   imageBaseUrl: 'https://image.tmdb.org/t/p',
-  apiKey: '4a3e57ea2256361cc9c6796fd38fda4b',
+  apiKey: process.env.REACT_APP_API_KEY,
 };
